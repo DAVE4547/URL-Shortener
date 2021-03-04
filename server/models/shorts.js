22296050f5fe8dda8a4d213cb46ccc7f46f55922
DAVE4547 <benjaminlovering1@gmail.com>
2021-03-04 13:36:06 +0000

    added short redirect

--- server/models/shorts.js
+++ server/models/shorts.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 require('mongoose-type-url')
 
 const shortsSchema = new mongoose.Schema({
-    long: mongoose.SchemaTypes.Url,
-    shortID: String,
+    long: { type: mongoose.SchemaTypes.Url, required: true },
+    shortID: { type: String, required: true, unique: true},
     date: { type: Date, default: Date.now() },
     review: {
         reviewed: { type: Boolean, default: false },
--- server/router/web.js
+++ server/router/web.js
@@ -11,7 +11,14 @@ web.get('/legal', (req, res) => {
     res.sendFile('legal.html', { root: './public' })
 })
 web.get('/404', (req, res) => {
-    res.status('404')
+    res.status(404)
+})
+
+const shorts = require('./../models/shorts')
+
+web.get('/:shortID', async (req, res) => {
+    var doc = await shorts.findOne({ shortID: req.params.shortID }).exec()
+    res.redirect(doc.long)
 })
 
 module.exports = web
